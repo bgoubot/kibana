0a2f3886c37137ebd959ef127eede76d0689ba73
bgoubot <bgoubot@yahoo.fr>
2017-01-16 13:54:02 +0100

    add env HOST SRVNAME

--- build/kibana/5/docker-entrypoint.sh
+++ build/kibana/5/docker-entrypoint.sh
@@ -9,14 +9,20 @@ fi
 # Run as user "kibana" if the command is "kibana"
 if [ "$1" = 'kibana' ]; then
         if [ "$ELASTICSEARCH_URL" ]; then
-                sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibana/config/kibana.yml
+                sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /etc/kibana/kibana.yml
         fi
         if [ "$KIBANA_INDEX" ]; then
-                sed -ri "s!^(\#\s*)?(kibana\.index:).*!\2 ${KIBANA_INDEX}!" /opt/kibana/config/kibana.yml
-        fi
-        if [ "$KIBANA_BASEPATH" ]; then
-                sed -ri "s!^(\#\s*)?(server\.basePath:).*!\2 ${KIBANA_BASEPATH}!" /opt/kibana/config/kibana.yml
-        fi
+                sed -ri "s!^(\#\s*)?(kibana\.index:).*!\2 ${KIBANA_INDEX}!" /etc/kibana/kibana.yml
+    	fi
+    	if [ "$KIBANA_BASEPATH" ]; then
+        	sed -ri "s!^(\#\s*)?(server\.basePath:).*!\2 ${KIBANA_BASEPATH}!" /etc/kibana/kibana.yml
+    	fi
+    	if [ "$KIBANA_HOST" ]; then
+        	sed -ri "s!^(\#\s*)?(server\.host:).*!\2 ${KIBANA_HOST}!" /etc/kibana/kibana.yml
+    	fi
+    	if [ "$KIBANA_SRVNAME" ]; then
+        	sed -ri "s!^(\#\s*)?(server\.name:).*!\2 ${KIBANA_SRVNAME}!" /etc/kibana/kibana.yml
+   	fi
 
         set -- gosu kibana tini -- "$@"
 fi
