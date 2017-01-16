918902a10f97e15ef4635c0477d758610d75bd69
bgoubot <bgoubot@yahoo.fr>
2017-01-16 13:49:54 +0100

    update with env index / basepath

--- build/kibana/5/docker-entrypoint.sh
+++ build/kibana/5/docker-entrypoint.sh
@@ -8,11 +8,17 @@ fi
 
 # Run as user "kibana" if the command is "kibana"
 if [ "$1" = 'kibana' ]; then
-	if [ "$ELASTICSEARCH_URL" ]; then
-		sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /etc/kibana/kibana.yml
-	fi
+        if [ "$ELASTICSEARCH_URL" ]; then
+                sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibana/config/kibana.yml
+        fi
+        if [ "$KIBANA_INDEX" ]; then
+                sed -ri "s!^(\#\s*)?(kibana\.index:).*!\2 ${KIBANA_INDEX}!" /opt/kibana/config/kibana.yml
+        fi
+        if [ "$KIBANA_BASEPATH" ]; then
+                sed -ri "s!^(\#\s*)?(server\.basePath:).*!\2 ${KIBANA_BASEPATH}!" /opt/kibana/config/kibana.yml
+        fi
 
-	set -- gosu kibana tini -- "$@"
+        set -- gosu kibana tini -- "$@"
 fi
 
 exec "$@"
