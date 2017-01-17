9ccea8f81c5a00fbb7d71868b8dfcf9895738425
bgoubot <bgoubot@yahoo.fr>
2017-01-17 10:54:08 +0100

    Update instal-plugin.sh

--- build/kibana/5/instal-plugin.sh
+++ build/kibana/5/instal-plugin.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-cd /usr/share/kibana/plugins \
-git clone https://github.com/bgoubot/kbn_c3js_vis.git c3_charts \
-cd c3_charts \
-npm install
+#cd /usr/share/kibana/plugins \
+#git clone https://github.com/bgoubot/kbn_c3js_vis.git c3_charts \
+#cd c3_charts \
+#npm install
+#plugin prealert
+kibana-plugin install https://github.com/prelert/kibana-swimlane-vis/releases/download/v5.0.1/prelert_swimlane_vis-5.0.1.zip
